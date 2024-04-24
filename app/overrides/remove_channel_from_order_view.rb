5f84cbe15a12a798d05304e9109da08f120cabd5
TomaszKot11 <kottomasz98@gmail.com>
2024-04-24 14:12:40 +0200

    Remove channel

---
+++ app/overrides/remove_channel_from_order_view.rb
@@ -0,0 +1,3 @@
+Deface::Override.new(virtual_path: 'spree/admin/shared/_order_summary',
+  name: 'remove_channel_from_order_view',
+  remove: ".list-group-item.d-flex.justify-content-between.align-items-centerd")
